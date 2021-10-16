2338ee70dfd34b21cdb9bdc5f464d08838148551
Yash Sanghvi <56947924+yashsanghvi001@users.noreply.github.com>
2021-10-16 18:15:53 +0530

    Add files via upload

--- Module-2/Code/Array/Array/Program.cs
+++ Module-2/Code/Array/Array/Program.cs
@@ -32,8 +32,32 @@ namespace Array
                 Console.WriteLine();
 
             }
+         Console.WriteLine();
 
-            Console.ReadLine();
+        // Declare the Jagged Array of four elements
+        int[][] jagged_arr = new int[4][];
+  
+        // Initialize the elements
+        jagged_arr[0] = new int[] {1, 2, 3, 4};
+        jagged_arr[1] = new int[] {11, 34, 67};
+        jagged_arr[2] = new int[] {89, 23};
+        jagged_arr[3] = new int[] {0, 45, 78, 53, 99};
+  
+        // Display the array elements:
+        for (int n = 0; n < jagged_arr.Length; n++) {
+  
+            // Print the row number
+            System.Console.Write("Row({0}): ", n);
+  
+            for (int k = 0; k < jagged_arr[n].Length; k++) {
+  
+                // Print the elements in the row
+                System.Console.Write("{0} ", jagged_arr[n][k]);
+            }
+            System.Console.WriteLine();
+        }
+
+        Console.ReadLine();
         }
     }
 }
