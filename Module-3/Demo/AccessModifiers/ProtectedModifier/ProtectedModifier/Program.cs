6f43f3270b7372233692a6540a58d33a386d30ad
Yash Sanghvi <56947924+yashsanghvi001@users.noreply.github.com>
2021-11-01 21:06:49 +0530

    Add files via upload

---
+++ Module-3/Demo/AccessModifiers/ProtectedModifier/ProtectedModifier/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProtectedModifier
+{
+	class Name
+	{
+
+		// Member name declared as protected
+		protected string name = "Yash Sanghvi";
+
+		public Name()
+		{
+			name = "Yash Sanghvi";
+		}
+	}
+
+	// class MyName inherits name
+	class MyName : Name
+	{
+
+		public string getName()
+		{
+			return name;
+		}
+	}
+	class Program
+    {
+        static void Main(string[] args)
+        {
+			Name obj1 = new Name();
+			MyName obj2 = new MyName();
+
+			// Displaying the value of name
+			Console.WriteLine("Name is : {0}", obj2.getName());
+			Console.Read();
+		}
+	}
+}
---
+++ Module-3/Demo/AccessModifiers/PublicModifier/PublicModifier/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PublicModifier
+{    // Declaring members rollNo and name as public
+    class Student
+    {
+        public int rollNo;
+        public string name;
+        //constructor
+        public Student(int r, string n)
+        {
+            rollNo = r;
+            name = n;
+        }
+        // method getName declared as public
+        public string getName()
+        {
+            return name;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {            // Creating object of the class Student
+            Student objStudent = new Student(10, "Yash");
+            // Displaying details directly using the class members accessible through another method
+            Console.WriteLine("Roll number: {0}", objStudent.rollNo);
+            Console.Write("Name: {0}", objStudent.name);
+            Console.WriteLine();
+            Console.WriteLine("Name: {0}", objStudent.getName());
+            Console.Read();
+
+        }
+    }
+}
