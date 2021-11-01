ab0f01daa49ece522c24124160d2b064ac7bd2ee
Yash Sanghvi <56947924+yashsanghvi001@users.noreply.github.com>
2021-11-01 21:04:41 +0530

    Add files via upload

---
+++ Module-3/Demo/EventDemo/EventDemo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventDemo
+{    //declare a 'MyDel' delegate 
+    public delegate string MyDel(string str);
+    class Event_demo
+    {
+        //declare the 'MyEvent' event of delagate 'MyDel'
+        event MyDel MyEvent;
+        public Event_demo()
+        {
+            this.MyEvent += new MyDel(this.WelcomeUser);
+        }
+        public string WelcomeUser(string username)
+        {
+            return "Hello, Welcome" + username + "To RKIT Softwere";
+        }
+    }
+        class Program
+    {
+        static void Main(string[] args)
+            {            //creating a object of EventProgram
+                Event_demo objEvent = new Event_demo();
+                string result = objEvent.MyEvent("Yash Sanghvi");
+                Console.WriteLine(result);
+                Console.Read();
+            }
+    }
+}
---
+++ Module-3/Demo/PropertiesDemo/PropertiesDemo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertiesDemo
+{
+    class student
+    {
+        //Getter and setter is used to set or get value.
+        public static string name { get; set; }
+        public static int age { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        { //set the value.
+            student.name = "Yash";
+            student.age = 20;
+
+            //get the value.
+            Console.WriteLine("Name : " + student.name);
+            Console.WriteLine("Age : " + student.age);
+            Console.ReadLine();
+        }
+    }
+}
