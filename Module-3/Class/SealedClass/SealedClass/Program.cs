6301ae47a41e7372e7415855e271e2f54f11fa4b
Yash Sanghvi <56947924+yashsanghvi001@users.noreply.github.com>
2021-11-01 21:01:54 +0530

    Add files via upload

---
+++ Module-3/Class/SealedClass/SealedClass/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SealedClass
+{
+    sealed public class PersonalInfo
+    {
+        public void Firstname()
+        {
+            Console.WriteLine("Yash");
+            Console.ReadLine();
+        }
+        public void Lastname()
+        {
+            Console.WriteLine("Sanghvi");
+            Console.ReadLine();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PersonalInfo objPersonalInfo = new PersonalInfo();
+            objPersonalInfo.Firstname();
+            objPersonalInfo.Lastname();
+        }
+    }
+}
