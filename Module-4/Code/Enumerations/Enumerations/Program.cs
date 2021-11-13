cc185a161b8ca68c6c2117a1b11b8b2e9442877b
Yash Sanghvi <56947924+yashsanghvi001@users.noreply.github.com>
2021-11-13 16:08:13 +0530

    Add files via upload

---
+++ Module-4/Code/Enumerations/Enumerations/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enumerations
+{
+    enum months
+    { // enum by default starts form 0 
+        January,  //0
+        February, //1
+        March,    //2
+        April,    //3
+        May,      //4
+        June =10,  // explicitly defined value of June is 10
+        July    
+
+    }
+
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // getting the integer values of data members..
+            Console.WriteLine("The value of January in month " +
+                              "enum is " + (int)months.January);
+            Console.WriteLine("The value of February in month " +
+                              "enum is " + (int)months.February);
+            Console.WriteLine("The value of March in month " +
+                              "enum is " + (int)months.March);
+            Console.WriteLine("The value of April in month " +
+                              "enum is " + (int)months.April);
+            Console.WriteLine("The value of June in month " +
+                              "enum is " + (int)months.June);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Module-4/Code/Interface/Interface/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface
+{
+    interface vehical // define Interface
+    {
+        void Tyers();
+        void Engine();
+        void Speed();
+    }
+    class Car : vehical // Inherited Interface
+    {
+        public void Tyers()
+        {
+            Console.WriteLine("Car Have 4 Tyers");
+        }
+        public void Engine()
+        {
+            Console.WriteLine("Car is Petrol Engine");
+
+        }
+        public void Speed()
+        {
+            Console.WriteLine("Car's Maximum Speed is 200KMPH");
+
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car ObjCar = new Car();
+            ObjCar.Tyers();
+            ObjCar.Engine();
+            ObjCar.Speed();
+
+            Console.Read();
+        }
+    }
+}
