cf492ec579e12e89b707edd113ba0a7ef976a08b
Yash Sanghvi <56947924+yashsanghvi001@users.noreply.github.com>
2021-11-13 16:10:56 +0530

    Updated Module 4

---
+++ Module-4/Code/File Operations/StremReader/StremReader/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace StremReader
+{
+    class Program
+    {
+        public void Data()
+        {
+            StreamReader objReader = new StreamReader("C://Users//yashs//Desktop//Module - 4//code//File Operations//Demo_Duplicate.txt");//it will read from the file given in the location
+
+            Console.WriteLine("Text in the file is :");
+            //this takes user input in string variable
+            string str = objReader.ReadLine();
+            while (str != null)
+            {
+                Console.WriteLine(str);
+                str = objReader.ReadLine();
+            }
+            objReader.Close();//stream is closed
+
+        }
+        static void Main(string[] args)
+        {
+            Program objProgram = new Program();
+            objProgram.Data();
+
+            Console.Read();
+        }
+    }
+}
---
+++ Module-4/Code/File Operations/StremWriterDemo/StremWriterDemo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace StremWriterDemo
+{
+    class Program
+    {
+        public void Data()
+        {
+            // write in the file given in the location
+            StreamWriter objWriter = new StreamWriter("C: //Users//yashs//Desktop//Module - 4//code//File Operations//StreamWriter.txt");
+
+            Console.WriteLine("Enter text to write in demo file:");
+            string str = Console.ReadLine();
+            objWriter.WriteLine(str);
+            objWriter.Flush();
+            //closes the stream and stream object
+            objWriter.Close();
+        }
+        static void Main(string[] args)
+        {
+            Program objProgram = new Program();
+            objProgram.Data();
+            Console.Read();
+        }
+    }
+}
