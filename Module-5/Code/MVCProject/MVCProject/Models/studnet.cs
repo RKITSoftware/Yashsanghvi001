a2cbb29d45727dd9516ca1adb1cc949a07fd396f
Yash Sanghvi <56947924+yashsanghvi001@users.noreply.github.com>
2021-12-01 10:20:50 +0530

    Add files via upload

---
+++ Module-5/Code/MVCProject/MVCProject/Models/studnet.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCProject.Models
+{
+    public class studnet
+    {
+        public int StudentId { get; set; }
+        public string StudentName { get; set; }
+        public int Age { get; set; }
+
+    }
+}
