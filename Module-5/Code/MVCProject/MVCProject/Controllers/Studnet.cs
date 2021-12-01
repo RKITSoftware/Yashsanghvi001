1105c375f6ac50a76f3682802a91bcac96ffbc11
Yash Sanghvi <56947924+yashsanghvi001@users.noreply.github.com>
2021-12-01 09:17:13 +0530

    Add files via upload

---
+++ Module-5/Code/MVCProject/MVCProject/Controllers/Studnet.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCProject.Controllers
+{
+    public class Studnet : Controller
+    { 
+    
+            // GET: Music
+            public ActionResult Index()
+            {
+                return View();
+            }
+            public string Welcome()
+            {
+                return "Hello, Weclome Studnet";
+            }
+            public string ShowInfo(string StudnetName)
+            {
+                return "You Secured " + StudnetName + " First Position";
+            }
+
+        
+    }
+
+
+
+}
