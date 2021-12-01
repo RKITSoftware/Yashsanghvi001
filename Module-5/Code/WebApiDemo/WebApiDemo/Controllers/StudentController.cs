6eceda426421d6e0067005cbb5784d720410c45e
Yash Sanghvi <56947924+yashsanghvi001@users.noreply.github.com>
2021-12-01 10:22:27 +0530

    Add files via upload

---
+++ Module-5/Code/WebApiDemo/WebApiDemo/Controllers/StudentController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace WebApiDemo.Controllers
+{
+    public class StudentController : ApiController
+    {
+
+            [Route("api/student/names")]
+            public IEnumerable<string> Get()
+            {
+                return new string[] { "student1", "student2" };
+            }
+
+
+}
+}
